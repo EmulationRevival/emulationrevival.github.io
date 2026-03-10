15bdecb4e0a309bb3e92a2c9e43583871f6ca93e
EmulationRevival <emulationrevival.github.io@noreply.github.com>
2026-03-10 19:59:48 +0000

    Optimise service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,8 @@
-// sw.js
+// sw.js 
+self.addEventListener('install', event => { 
+    self.skipWaiting(); 
+}); 
 
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('activate', event => { 
+    event.waitUntil(self.clients.claim()); 
 });
